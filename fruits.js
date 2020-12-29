c2f1eb4b4a7f8382b2405ad25ef41868c63d8e98
Nick Satinover <nsatinover.aws@gmail.com>
2020-12-28 22:41:32 -0500

    added style/ formatting to list

--- fruits.js
+++ fruits.js
@@ -4,19 +4,31 @@ const divHeader = document.querySelector('#headerDiv');
 
 
 // insert a div element above current body content
-let tocElem = document.createElement('ol');
+let tocElem = document.createElement('div');
 tocElem.id = 'toc';
 bodyElem.insertBefore(tocElem, divHeader);
 
 
-// jquery function on document ready to get chile elem of #contents and interate
+// jquery function on document ready to get child elem of #contents and iterate
+//console.log(divList[i].innerText);
 $(document).ready(function toc_builder(){
+    
     const divList = $('#contents').children();
 
     for (let i = 0; i < divList.length; i++) {
-        $('#toc').append(`<li>${divList[i].innerText}</li>`);
-        console.log(divList[i].innerText);
+        if(divList[i].nodeName == "H1"){
+            $('#toc').append(`<h1>${divList[i].innerText}</h1>`);
+            
+        } 
+        else if(divList[i].nodeName == "H2"){
+            $('#toc').append(`<ul><u>${divList[i].innerText}</u></ul>`);
+        }
+        else {
+            $('#toc').append(`<li>${divList[i].innerText}</li>`);
+        }
     }
+    $('#toc').append(`</br>`);
+    $('#toc').append(`</br>`);
 
 });
 
