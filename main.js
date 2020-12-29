3d468e1ef2fab680f7b4bcd84271681439f4d0bf
Amatullah Brown <amatullahbrown@Amatullahs-MacBook-Pro.local>
2020-12-28 22:29:53 -0500

    updated list information

---
+++ main.js
@@ -0,0 +1,6 @@
+const lis = document.querySelectorAll("li");
+lis[0].textContent = "Nickname: none";
+lis[1].textContent = "Favorites: baking, gardening, pottery";
+lis[2].textContent = "Hometown: Philadelphia";
+lis[3].textContent = "Random fact: Oprah loves bread.";
+document.body.style.fontFamily = "Arial", sans-ServiceUIFrameContext;
