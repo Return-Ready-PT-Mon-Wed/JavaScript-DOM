2a750e2ad360b7e564f3e1e64c6a61d700a41f5c
Amarachukwu Ugoanusi <amarachukwuugoanusi@MacBook-Air.local>
2020-12-28 21:04:03 -0500

    added the image

--- javaScript.js
+++ javaScript.js
@@ -5,8 +5,22 @@
 // var node2 = document.createTextNode("We are coders!");
 // para1.appendChild(node1);
 // para2.appendChild(node2);
+ 
 
+var html = "";
 
 document.getElementById("para1").innerHTML = "We are coders";
 document.getElementById("para2").innerHTML = "We are coders";
 document.getElementById("para1").innerHTML = "Developers for life!";
+
+html = document.getElementById("para1").innerHTML = "Developers for life!";
+
+var img = document.createElement('img');
+img.src = "lib/images/Kimberly Bryant.jpg";
+var src = document.querySelector("body");
+ src.appendChild(img);
+console.log(html);
+
+ 
+ 
+
