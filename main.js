af6c0520a046b2634ac3b9a8af05b12a8bb8bf02
Nick Satinover <nsatinover.aws@gmail.com>
2020-12-28 21:07:16 -0500

    added picture

--- main.js
+++ main.js
@@ -8,3 +8,16 @@ for (let i = 0; i < liElem.length; i++) {
     const thisLi = liElem[i];
     thisLi.style.color = "red";
 }
+
+//Add image
+let imageHtml = 
+`
+<h3>This is a picture of me!</h3>
+<img class="image" id="laptopImg" src="lib/images/Headshot Photo.JPG" alt="Granville Woods"/>
+`;
+
+bodyElem.innerHTML += imageHtml;
+
+let imgElem = document.querySelector('.image');
+imgElem.width = "100";
+
