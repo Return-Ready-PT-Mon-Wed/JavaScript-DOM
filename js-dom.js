e191396e43d9264f87e018426fc4feb1a77a88fe
Nick Satinover <nsatinover.aws@gmail.com>
2020-12-28 20:27:36 -0500

    paragraphs and image

--- js-dom.js
+++ js-dom.js
@@ -1,7 +1,29 @@
-let paraHtml = 
-`
-<p class="para" id="paraOne">one</p>
-<p class="para" id="paraTwo">two</p>
+//// Change text of p1, p2 ////
+const paraBothMessage = "We are coders!";
+const paraOneMessage = "Developers for life!";
+
+var bothPara = document.querySelectorAll(".para");
+
+for (let i = 0; i < bothPara.length; i++) {
+  const thisPara = bothPara[i];
+  thisPara.innerText = paraBothMessage;
+}
+
+var paraOne = document.getElementById("paraOne");
+paraOne.innerText = paraOneMessage;
+
+
+let imageHtml = 
+`<div class="tech">
+<div class="techie">
+  <h3>This is a laptop!</h3>
+  <img
+    class="image"
+    src="lib/images/laptop.svg"
+    alt="Granville Woods"
+  />
+</div> */
 `;
 
-document.querySelector('body').innerHTML = paraHtml;
+let bodyElem = document.querySelector('body');
+bodyElem.innerHTML += imageHtml;
