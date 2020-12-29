1396a54704567eb9ba42d655f65072cb48510fc0
Amarachukwu Ugoanusi <amarachukwuugoanusi@MacBook-Air.local>
2020-12-28 20:16:05 -0500

    commit 2

---
+++ javaScript.js
@@ -0,0 +1,12 @@
+// const paragraph1 = document.getElementById('para1');
+// const paragraph2 = document.getElementById('para2');
+// var paragraph1 = para1.textContent('We are coders!');
+// var node1 = document.createTextNode("We are coders!");
+// var node2 = document.createTextNode("We are coders!");
+// para1.appendChild(node1);
+// para2.appendChild(node2);
+
+
+document.getElementById("para1").innerHTML = "We are coders";
+document.getElementById("para2").innerHTML = "We are coders";
+document.getElementById("para1").innerHTML = "Developers for life!";
