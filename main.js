8068d20a6b976569b70bb6172994c37f8aff1c5e
Amatullah Brown <amatullahbrown@Amatullahs-MacBook-Pro.local>
2020-12-28 22:35:39 -0500

    changed li color

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ lis[0].textContent = "Nickname: none";
 lis[1].textContent = "Favorites: baking, gardening, pottery";
 lis[2].textContent = "Hometown: Philadelphia";
 lis[3].textContent = "Random fact: Oprah loves bread.";
+// change <li> color
+for (let i=0; i<lis.length; i++) {
+    lis[i].style.color = "darkgreen";
+}
+
 document.body.style.fontFamily = "Arial", sans-ServiceUIFrameContext;
