e5834355391579191613758dbc2f9a4b69de644e
Amarachukwu Ugoanusi <amarachukwuugoanusi@MacBook-Air.local>
2020-12-29 12:14:31 -0500

    final commit  for part 2

--- script2.js
+++ script2.js
@@ -5,15 +5,9 @@ var ul = document.createElement('ul');
 body.appendChild(ul);
 
 ul.innerHTML = "<span id = 'span1'><li>Nickname: booboo</li></span > <span id= 'span2'><li>Favorites: music, pizza, The Office</li></span><span id= 'span3'><li>Hometown: Wilmington </li> </span><span id = 'span4'><li>Random Fact: Black is my favorite color.</li></span>";
-var img = document.createElement('img');
-img.src = "nkenna.jpg";
-body.appendChild(img);
-img.style.width = '30%';
 
-var li = document.querySelectorAll('li');
-for (let i=0 ; i < li.length; i +=1){
-    li[i].style.color = 'red';  
-}
+
+
 
 // "<ul>
 //     <li>Nickname: booboo</li>
