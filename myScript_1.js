20ec6f006a476e5f4ab6e05a3454254a0fbc6896
Stephanie Czetli <stephanieczetli@gmail.com>
2021-01-02 15:11:56 -0500

    completes Part One

--- myScript_1.js
+++ myScript_1.js
@@ -1,13 +1,41 @@
-// Add "We are coders!" to the `<p>` element
-document.getElementById("para-1").innerHTML = "We are coders!";
-document.getElementById("para-2").innerHTML = "We are coders!";
 
-//Change text in first `<p>` element to "Developers for life!"
-document.getElementById("para-1").innerHTML = "Developers for life!";
+const msgBothPars = "We are coders!";
+const msgFirstPar = "Developers for life!";
 
-//Add image to HTML document using DOM methods
-var img = document.createElement('img');
-img.src = "diversetech.jpeg";
-var body = document.querySelector("body");
-body.appendChild(img);
+//Insert text to both paragraphs
+let bothPars = document.querySelectorAll(".pars");
+
+for (let i = 0; i < bothPars.length; i++) {
+    const thisPars = bothPars[i];
+    thisPars.innerText = msgBothPars;
+};
+
+//Change text content & font size of first paragraph
+let parFirst = document.getElementById("parOne");
+parOne.innerText = msgFirstPar;
+parOne.style.fontSize = "40px";
+
+//Assign image to variable
+let imageHtml = `
+<img id="image" class="divTechImg" src="lib/images/diversetech.jpeg" alt="drawing of diverse people in tech"></img>
+`;
+
+//Place image in body of HTML
+let bodyTag = document.querySelector('body');
+bodyTag.innerHTML += imageHtml;
+
+//Change text color in body to purple
+bodyTag.style.color = "purple";
+
+//Create functionality to hide/display image with button
+function buttonAction() {
+    const targetImage = document.querySelector(".divTechImg");
+    const curStatus = targetImage.style.visibility;
+
+    if(curStatus == "hidden"){
+        targetImage.style.visibility = "visible";
+    } else {
+        targetImage.style.visibility = "hidden";
+    }
+}
 
